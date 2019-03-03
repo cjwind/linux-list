47b0b071e671d7e60b666a4f779fb9ebe4555b1c
cjwind <cwentsai@gmail.com>
2019-03-03 21:25:33 +0800

    Change finding pivot algorithm of quick sort
    Improve quick sort performance.

--- quick-sort.h
+++ quick-sort.h
@@ -1,6 +1,49 @@
 #include "list.h"
 #include "common.h"
 
+static struct listitem *find_pivot(struct list_head *head)
+{
+    struct listitem *first = NULL, *last = NULL, *mid = NULL;
+
+    first = list_first_entry(head, struct listitem, list);
+    last = list_last_entry(head, struct listitem, list);
+
+    struct list_head *node = NULL;
+    int count = 0;
+    list_for_each (node, head) {
+        count++;
+    }
+
+    int i = 0;
+    int mid_idx = count / 2 - 1;
+    list_for_each_entry (mid, head, list) {
+        if (i == mid_idx) {
+            break;
+        }
+        i++;
+    }
+
+    struct listitem *big = NULL, *small = NULL;
+
+    if (cmpint(&first->i, &last->i) > 0) {
+        big = first;
+        small = last;
+    } else {
+        big = last;
+        small = first;
+    }
+
+    if (cmpint(&mid->i, &big->i) > 0) {
+        return big;
+    }
+
+    if (cmpint(&mid->i, &small->i) > 0) {
+        return mid;
+    }
+
+    return small;
+}
+
 static void list_qsort(struct list_head *head)
 {
     struct list_head list_less, list_greater;
@@ -13,7 +56,7 @@ static void list_qsort(struct list_head *head)
     INIT_LIST_HEAD(&list_less);
     INIT_LIST_HEAD(&list_greater);
 
-    pivot = list_first_entry(head, struct listitem, list);
+    pivot = find_pivot(head);
     list_del(&pivot->list);
 
     list_for_each_entry_safe (item, is, head, list) {
